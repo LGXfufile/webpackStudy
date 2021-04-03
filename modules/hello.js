aa8f10155aeca2a77af038eaa58a1b5ae845708e
LGXfufile <401040990@qq.com>
2021-04-03 16:05:29 +0800

    Initial commit

---
+++ modules/hello.js
@@ -0,0 +1,10 @@
+// 暴露一个方法
+exports.sayhi = function (){
+    document.write("<h1>xinxin</h1>")
+}
+exports.sayhi1 = function (){
+    document.write("<h1>xinxin</h1>")
+}
+exports.sayhi2 = function (){
+    document.write("<h1>xinxin</h1>")
+}
---
+++ modules/main.js
@@ -0,0 +1,4 @@
+var hello = require("./hello");
+hello.sayhi()
+hello.sayhi1()
+hello.sayhi2()
